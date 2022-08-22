81ff13edc9d5de892f67bee42b34fc051c1286fb
Matvey2009 <matvey2009ua@gmail.com>
2022-08-22 21:33:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
-<? include_once('php/header.php'); ?>
+<? $db = 'model/sqliete';
+include_once('model/migration.php'); ?>
+<? include_once('view/header.php'); ?>
+
 <form method="post">
     <input type="submit" name="bet" value="Matvey">
     <input type="submit" name="reset" value="Krutoy">
 </form>
-<? include_once('php/footer.php'); ?>
+<? include_once('view/footer.php'); ?>
