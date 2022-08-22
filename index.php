1a438555c048254372b297f6a692e98e1ffb73a6
Matvey2009 <matvey2009ua@gmail.com>
2022-08-22 20:13:20 +0300

    Copy casino

---
+++ index.php
@@ -0,0 +1,6 @@
+<? include_once('php/header.php'); ?>
+<form method="post">
+    <input type="submit" name="bet" value="Matvey">
+    <input type="submit" name="reset" value="Krutoy">
+</form>
+<? include_once('php/footer.php'); ?>
