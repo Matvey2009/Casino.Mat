b7c2f84ea1d02e20bd507c0f0f62e77e0d4eb6f2
Matvey2009 <matvey2009ua@gmail.com>
2022-09-10 21:20:02 +0300

    Update roulette.php

--- model/roulette.php
+++ model/roulette.php
@@ -1,2 +1,28 @@
 <?php
+
     require_once("model/model.php");
+
+    class Roulette extends Model {
+
+        private $sql;
+
+        protected function __construct(){
+            parent::__construct();
+            $this-> sql = '';
+        }
+
+        //Добавляем статистику в дб
+
+        //Брать данные с дб
+
+        //Обнулить
+        public function reset() {
+            if (array_key_exists('reset', $_POST)){
+                $this->sql = "
+                    UPDATE `Roulette` SET stat = 0
+                ";
+            }
+            $this -> inDB($this->sql);
+        }
+    }
+?>
