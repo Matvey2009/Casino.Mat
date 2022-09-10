267dfb79d0040e272b8a9cd6dad2755fa9e3d81d
Matvey2009 <matvey2009ua@gmail.com>
2022-09-10 21:20:04 +0300

    Update model.php

--- model/model.php
+++ model/model.php
@@ -1,3 +1,34 @@
 <?php
-$db = 'model/data/casino.sqlite';
-//require_once('model/migration.php');
+abstract class Model {
+    
+    private $path;
+    private $connect;
+
+    protected function __construct(){
+        $this -> path = 'model/data/casino.sqlite';
+    }
+
+    // Внести из базы даных
+    protected function inDB($sql){ 
+        try {
+            $this -> connect = new PDO("sqlite:$this");
+            $this -> connect -> exec($sql);
+            $this -> connect = null;
+        } catch(PDOException $e){
+            echo "Date base error". $e -> getMessage();
+        }
+    }
+
+    // Извечть из базы даных
+    protected function outDB($sql){
+        try {
+            $this -> connect = new PDO("sqlite:$this");
+            $request = $this -> connect -> query($sql);
+            $this -> connect = null;
+            return $request;
+        } catch(PDOException $e){
+            echo "Date base error". $e -> getMessage();
+            return null;
+        }
+    }
+}
