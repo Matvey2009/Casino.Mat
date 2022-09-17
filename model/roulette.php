1b97e1c1aa04302f2eace3b9b3adfb391583cbe9
Matvey2009 <matvey2009ua@gmail.com>
2022-09-17 18:10:05 +0300

    Update roulette.php

--- model/roulette.php
+++ model/roulette.php
@@ -12,16 +12,30 @@
         }
 
         //Добавляем статистику в дб
+        public function setStat($sector) {
+            $this->sql = "
+                UPDATE `Roulette` SET stat=stat+1 WHERE sector = $sector
+            ";
+            $this -> inDB($this->sql);
+        }
 
         //Брать данные с дб
+        public function gerStat() {
+            $this->sql = "
+                SELECT `sector`, `stat` FROM `Roulette` ORDER BY `sector`
+            ";
+            $request = $this -> outDB($this->sql);
+            $stat = [];
+            foreach($request as $row)
+                $stat[$row['sector']] = $row['stat'];
+            return $stat;
+        }
 
         //Обнулить
-        public function reset() {
-            if (array_key_exists('reset', $_POST)){
-                $this->sql = "
-                    UPDATE `Roulette` SET stat = 0
-                ";
-            }
+        public function resetStat() {
+            $this->sql = "
+                UPDATE `Roulette` SET stat = 0
+            ";
             $this -> inDB($this->sql);
         }
     }
