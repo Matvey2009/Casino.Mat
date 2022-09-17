348ec591ebce2038ebbdb0527ac8e3a7af3164fd
Matvey2009 <matvey2009ua@gmail.com>
2022-09-17 18:10:06 +0300

    Update user.php

--- model/user.php
+++ model/user.php
@@ -1,2 +1,28 @@
 <?php
     require_once("model/model.php");
+
+    class User extends Model {
+
+        private $sql;
+
+        protected function __construct(){
+            parent::__construct();
+            $this-> sql = '';
+        }
+
+        //Добавляем игрока
+        public function addUser(...) {
+            $this->sql = "
+                
+            ";
+            $this -> inDB($this->sql);
+        }
+
+        //Обновляем счёт игрока
+
+        //Удаляем игрока
+
+        //Редактирование игрока
+
+    }
+?>
